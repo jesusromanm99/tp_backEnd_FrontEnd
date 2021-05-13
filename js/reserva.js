b84f5adb753e6ff8ec5677e385a1b6b93e4870da
David Nuñez <nundavid99@gmail.com>
2021-05-13 00:37:40 -0400

    reserva refactoring

--- js/reserva.js
+++ js/reserva.js
@@ -1,6 +1,55 @@
 const ENDPOINT="http://localhost:9090/"
 
-var app= new Vue({
+const get_todos_restaurantes = async()=>{
+    const res = await fetch(`${ENDPOINT}api/restaurant/`);
+    if(res){
+        const data = res.json();
+        return data;
+    }
+}
+
+const get_mesas_disponibles=(mesas,mesas_ocupadas)=>{
+    mesas_disponibles=[]
+    //console.log(mesas)
+    //console.log(mesas_ocupadas)
+    mesas.forEach(element => {
+        var mesa= mesas_ocupadas.find( valor =>{
+            //console.log("1-",valor.id_mesa)
+            //console.log("2-",element.id)
+            return valor.id_mesa==element.id
+        })
+        //console.log(mesa)
+        if(!mesa){
+            mesas_disponibles.push(element)
+        }
+    });
+    //console.log("Estas son todas las mesas disponibles",mesas_disponibles)
+    return mesas_disponibles;
+}
+const get_mesas_ocupadas= async (id_res,fecha,hora_inicio,hora_fin)=>{
+    date=fecha
+    params=id_res+"/"+date+"/"+hora_inicio+"/"+hora_fin
+    res= await fetch(ENDPOINT+"api/reserva/mesas/"+params)
+    if(res) return res.json()
+}
+const get_todas_mesas= async(mesas_ocupadas)=> {
+    mesas= await fetch(ENDPOINT+"api/mesa")
+    if( mesas){
+        return mesas.json()
+    }
+    
+}
+const get_date_correct_form= (date)=>{
+    newDate=date.split("-")
+    return newDate[0]+"-"+newDate[2]+"-"+newDate[1];
+}
+const get_date_correct_form_post= (date)=>{
+    newDate=date.split("-")
+    return newDate[1]+"-"+newDate[2]+"-"+newDate[0];
+}
+
+
+const app= new Vue({
     el:"#app",
     data:{
         reserva:{
@@ -12,16 +61,7 @@ var app= new Vue({
                     id_cliente:"",
                     cantidad_solicitada:"",
         },
-        restaurantes:[
-            {
-                id:1,
-                nombre:"Restaurant1"
-            },
-            {
-                id:2,
-                nombre:"Restaurant2"
-            }
-        ],
+        restaurantes:[],
         rango_horas_inicio:[12,13,14,15,19,20,21,22],
         rango_horas_fin:[13,14,15,20,21,22,23],
         hora_inicio:null,
@@ -39,7 +79,11 @@ var app= new Vue({
         },
         findCedula:true ,
         reservaLista:false ,
-        mensaje_error:null
+        mensaje_error:null,
+        mensaje_exito:null,
+    },
+    beforeCreate: async function() {
+        this.restaurantes = await get_todos_restaurantes();
     },
     methods:{
         buscar_reservas:async function(){
@@ -62,7 +106,7 @@ var app= new Vue({
             }
             
          },
-         guardar_reserva: function(){
+        guardar_reserva: function(){
                 //crear el cliente
                 this.settearReserva()
                 console.log("reserva:",JSON.stringify(this.reserva))
@@ -76,11 +120,16 @@ var app= new Vue({
                    if(response.ok){return response.json}
                   else { throw "Erro al hacer el post de reserva"}
                  })
-                .then(data=> console.log("Se creo la Reserva",data))
+                .then(data=>{
+                     console.log("Se creo la Reserva",data);
+                     //this.findCedula = false;
+                     //this.reservaLista = false;
+                     this.mensaje_exito = "Se creo la Reserva";
+                    })
                 .catch(err=>console.log(err))
                 //crea y confirma una reserva
          },
-         buscar_cedula: async function(){
+        buscar_cedula: async function(){
             if(this.cliente.cedula!=null){
                 this.mensaje_error=null 
                 cliente= await fetch(ENDPOINT+"api/cliente/cedula/"+this.cliente.cedula)
@@ -105,99 +154,59 @@ var app= new Vue({
             }
             
          },
-         confirmar_datos: function(){
-            //crear el cliente por debajo
-           if(this.cliente.nombre==null || this.cliente.apellido==null){
-                this.mensaje_error="Complete bien los campos"
-           }
-           else if(this.mesaObject==null){
-               this.mensaje_error="Seleccione una de las mesas disponibles para realizar la reserva"
-           }
-           else{
-                // console.log("a2",JSON.stringify(this.cliente))
-                this.mensaje_error=null
-                fetch(ENDPOINT+"api/cliente/", {
-                    method: 'POST',
-                    body:JSON.stringify(this.cliente),
-                    headers:{
-                        'Content-Type': 'application/json'
-                    }
-                }).then(response =>{
-                    if(response.ok){return response.json()}
-                    else { throw "Erro al hacer el post de cliente"}
-                })
-                .then(data=> {
-                    this.cliente=data; 
-                    console.log("Se creo el cliente",data)
-                    })
-                .catch(err=>console.log(err))
-                console.log("a")
-                this.reservaLista=true
-                this.settearReserva()
-           }
-            
-         },
-         settearReserva:function(){
-            if(this.mesaObject==null){
-                this.mensaje_error="Seleccione una de las mesas disponibles para realizar la reserva"
-            }else{
-                this.mensaje_error=null
-                newReserva={
-                    fecha:get_date_correct_form_post(this.fecha),
-                    hora_inicial:this.hora_inicio,
-                    hora_final:this.hora_fin,
-                    id_restaurante:this.restaurant_index,
-                    id_mesa:this.mesaObject.id,
-                    id_cliente:this.cliente.id,
-                    cantidad_solicitada:this.mesaObject.capacidad
-                };
-                this.reserva=newReserva
-                this.reservaLista=true
-            }
-            
-         }
-        
-    }
-   
-})
-
-
-get_mesas_disponibles=(mesas,mesas_ocupadas)=>{
-    mesas_disponibles=[]
-    //console.log(mesas)
-    //console.log(mesas_ocupadas)
-    mesas.forEach(element => {
-        var mesa= mesas_ocupadas.find( valor =>{
-            //console.log("1-",valor.id_mesa)
-            //console.log("2-",element.id)
-            return valor.id_mesa==element.id
-        })
-        //console.log(mesa)
-        if(!mesa){
-            mesas_disponibles.push(element)
+        confirmar_datos: function(){
+        //crear el cliente por debajo
+        if(this.cliente.nombre==null || this.cliente.apellido==null){
+            this.mensaje_error="Complete bien los campos"
         }
-    });
-    //console.log("Estas son todas las mesas disponibles",mesas_disponibles)
-    return mesas_disponibles;
-}
-get_mesas_ocupadas= async (id_res,fecha,hora_inicio,hora_fin)=>{
-    date=fecha
-    params=id_res+"/"+date+"/"+hora_inicio+"/"+hora_fin
-    res= await fetch(ENDPOINT+"api/reserva/mesas/"+params)
-    if(res) return res.json()
-}
-get_todas_mesas= async(mesas_ocupadas)=> {
-    mesas= await fetch(ENDPOINT+"api/mesa")
-    if( mesas){
-        return mesas.json()
+        else if(this.mesaObject==null){
+            this.mensaje_error="Seleccione una de las mesas disponibles para realizar la reserva"
+        }
+        else{
+            // console.log("a2",JSON.stringify(this.cliente))
+            this.mensaje_error=null
+            fetch(ENDPOINT+"api/cliente/", {
+                method: 'POST',
+                body:JSON.stringify(this.cliente),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            }).then(response =>{
+                if(response.ok){return response.json()}
+                else { throw "Erro al hacer el post de cliente"}
+            })
+            .then(data=> {
+                this.cliente=data; 
+                console.log("Se creo el cliente",data)
+                })
+            .catch(err=>console.log(err))
+            console.log("a")
+            this.reservaLista=true
+            this.settearReserva()
+        }
+        
+        },
+        guardar_restaurant: async function(){
+            
+        },
+        settearReserva:function(){
+        if(this.mesaObject==null){
+            this.mensaje_error="Seleccione una de las mesas disponibles para realizar la reserva"
+        }else{
+            this.mensaje_error=null
+            newReserva={
+                fecha:get_date_correct_form_post(this.fecha),
+                hora_inicial:this.hora_inicio,
+                hora_final:this.hora_fin,
+                id_restaurante:this.restaurant_index,
+                id_mesa:this.mesaObject.id,
+                id_cliente:this.cliente.id,
+                cantidad_solicitada:this.mesaObject.capacidad
+            };
+            this.reserva=newReserva
+            this.reservaLista=true
+        }
+        
+        }
     }
-    
-}
-get_date_correct_form= (date)=>{
-    newDate=date.split("-")
-    return newDate[0]+"-"+newDate[2]+"-"+newDate[1];
-}
-get_date_correct_form_post= (date)=>{
-    newDate=date.split("-")
-    return newDate[1]+"-"+newDate[2]+"-"+newDate[0];
-}
+})
